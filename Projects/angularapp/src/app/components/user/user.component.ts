7cc180ce4d7066fc7b8d2303b745ac426d2facf5
MehdiZebarjadan <MehdiZebarjadan@yahoo.com>
2018-06-25 23:14:02 -0400

    Properties & Methods

--- Projects/angularapp/src/app/components/user/user.component.ts
+++ Projects/angularapp/src/app/components/user/user.component.ts
@@ -3,15 +3,29 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',  
-  styleUrls: ['./user.component.css'],
-  // template: '<h2>Mehdi Ze</h2>',
-  // styles: [`
-  //   h2 {
-  //     color:blue
-  //   }
-  // `]
+  styleUrls: ['./user.component.css']
 })
 
 export class UserComponent {
+  // Properties
+  firstName = 'Mehdi';
+  lastName = 'Ze.';
+  age = 38;
 
+  // Methods
+  constructor() {
+    // this.sayHello();
+    console.log(this.age);
+    this.hasBirthday();
+    console.log(this.age);
+
+  }
+
+  sayHello() {
+    console.log(`Hello ${this.firstName}`); //es6 new way to do >> ('Hello' + ' ' + this.firstName)
+  }
+
+  hasBirthday() {
+    this.age += 1;
+  }
 }
