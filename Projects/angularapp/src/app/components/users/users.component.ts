01bc2696e088cf6a1c49ea419737b38778816a03
MehdiZebarjadan <MehdiZebarjadan@yahoo.com>
2018-06-26 01:45:16 -0400

    Loops With ngFor

--- Projects/angularapp/src/app/components/users/users.component.ts
+++ Projects/angularapp/src/app/components/users/users.component.ts
@@ -1,16 +1,64 @@
 import { Component, OnInit } from '@angular/core';
 
+import { User } from '../../models/User';
+
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
   styleUrls: ['./users.component.css']
 })
 export class UsersComponent implements OnInit {
+  users: User[];
 
   constructor() { }
 
   ngOnInit() {
-    console.log('init...');
+    this.users = [
+      {
+        firstName: 'Mehdi',
+        lastName: 'Ze.',
+        age: 20,
+        address: {
+          street: '50 Main st',
+          city: 'Dallas',
+          state: 'TX'
+        }
+      },
+      {
+        firstName: 'Sam',
+        lastName: 'Ji',
+        age: 70,
+        address: {
+          street: '80 shoosh st',
+          city: 'San Jose',
+          state: 'CA'
+        }
+      },
+      {
+        firstName: 'Linda',
+        lastName: 'Bumzi',
+        age: 18,
+        address: {
+          street: '9 oslo st',
+          city: 'Lose Angeles',
+          state: 'CA'
+        }
+      }
+    ];
+
+    this.addUser({
+      firstName: 'Dany',
+      lastName: 'G',
+      age: 58,
+      address: {
+        street: '9 athabs st',
+        city: 'San Francisco',
+        state: 'CA'
+      }
+    });
   }
 
+  addUser(user: User) {
+    this.users.push(user);
+  }
 }
