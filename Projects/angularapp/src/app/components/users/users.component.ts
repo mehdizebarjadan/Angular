b09afc09883fa78635a22f9a7ef1aa0526abe00b
MehdiZebarjadan <MehdiZebarjadan@yahoo.com>
2018-06-27 17:09:27 -0400

    Template Driven Forms - Submission

--- Projects/angularapp/src/app/components/users/users.component.ts
+++ Projects/angularapp/src/app/components/users/users.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 import { User } from '../../models/User';
 
@@ -19,6 +19,7 @@ export class UsersComponent implements OnInit {
   loaded: boolean = false;
   enabledAdd: boolean = false;
   showUserForm: boolean = false;
+  @ViewChild('userForm') form: any;
 
   constructor() { }
 
@@ -53,20 +54,17 @@ export class UsersComponent implements OnInit {
     this.loaded = true;
   }
 
-  // addUser() {
-  //   this.user.isActive = true;
-  //   this.user.registered = new Date();
+  onSubmit({value, valid}: {value: User, valid: boolean}) {
+    if(!valid){
+      console.log('Form is not valid');
+    } else {
+      value.isActive = true;
+      value.registered = new Date();
+      value.hide = true;
 
-  //   this.users.unshift(this.user);
+      this.users.unshift(value);
 
-  //   this.user = {
-  //     firstName: '',
-  //     lastName: '',
-  //     email: ''
-  //   }
-  // }
-
-  onSubmit() {
-
+      this.form.reset();
+    }
   }
 }
